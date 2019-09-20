544ab231baef513df54103834adba7575cdf5de8
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2019-09-20 09:37:00 +0000

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:face_try/try.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FacePage(),
+      ),
+    );
